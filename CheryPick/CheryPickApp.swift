6e519fb3509f469b7b437ea1ece0df57b1f5045b
wahid tariq <wahid.tariq@anywhere.co>
2024-05-11 16:29:06 +0530

    Initial Commit changed

---
+++ CheryPick/CheryPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheryPickApp.swift
+//  CheryPick
+//
+//  Created by wahid tariq on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CheryPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
